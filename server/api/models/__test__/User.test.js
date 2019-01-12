b9bba6f0e78263504e8bd5642b6e8923eaa5e555
Tucker <trblackw@ncsu.edu>
2019-01-12 12:42:28 -0500

    wrote basic tests for user model

---
+++ server/api/models/__test__/User.test.js
@@ -0,0 +1,46 @@
+const User = require("../user");
+//TODO: test for unique username
+describe("User model", () => {
+  test("username is required", async () => {
+    expect.assertions(1);
+    try {
+      await User.create({
+        email: "student@gmail.com",
+        password: "f777aaa829",
+        city: "Raleigh",
+        state: "North Carolina",
+        zip: 27606
+      });
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+  test("password is required", async () => {
+    expect.assertions(1);
+    try {
+      await User.create({
+        username: "jimJamboJangles",
+        email: "student@gmail.com",
+        city: "Raleigh",
+        state: "North Carolina",
+        zip: 27606
+      });
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+  test("email is required", async () => {
+    expect.assertions(1);
+    try {
+      await User.create({
+        username: "jimJamboJangles",
+        password: "f777aaa829",
+        city: "Raleigh",
+        state: "North Carolina",
+        zip: 27606
+      });
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+});
