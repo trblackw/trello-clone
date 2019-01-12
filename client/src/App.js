8bbca38bc69e13e3bcfbd2bf70c083c6e758b6c8
Tucker <trblackw@ncsu.edu>
2019-01-12 17:22:19 -0500

    added base routes for login and landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-                Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import React, { useContext, useReducer } from "react";
+import { Router } from "@reach/router";
+import Login from "./components/Login";
+import Landing from "./components/Landing";
+import UserContext from "./state/context";
+import UserReducer from "./state/reducer";
+import Nav from "./components/Nav";
+const App = () => {
+  const initialState = useContext(UserContext);
+  const [{ currentUser }, dispatch] = useReducer(UserReducer, initialState);
+  return (
+    <>
+      <UserContext.Provider value={{ currentUser, dispatch }}>
+        <Nav />
+        <Router>
+          <Login path="/" />
+          <Landing path="/user/:id" />
+        </Router>
+      </UserContext.Provider>
+    </>
+  );
+};
 
 export default App;
