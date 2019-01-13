6ee3b70f589aca4d6f92e708b7c9aa145f63d412
Tucker <trblackw@ncsu.edu>
2019-01-13 12:27:06 -0500

    built out reducer that utilizes an initial state that looks for a user that's been previously set in localstorage to persist UX beyond browser refresh

---
+++ client/src/state/reducer.js
@@ -0,0 +1,50 @@
+import { REGISTER_USER, LOGIN_USER, LOGOUT_USER } from "./constants";
+
+const initialState = {
+  currentUser: localStorage.getItem("authenticated-user") || "",
+  token: localStorage.getItem("authenticated-user").token || "",
+  username: localStorage.getItem("authenticated-user").username || "",
+  errorMessage: ""
+};
+
+const UserReducer = (state = initialState, action) => {
+  const {
+    type,
+    registeredUser,
+    loggedInUser,
+    success,
+    token,
+    username,
+    message
+  } = action;
+  switch (type) {
+    case REGISTER_USER:
+      console.log(
+        `%c {type: REGISTER_USER, registeredUser: ${JSON.stringify(
+          registeredUser
+        )}} `,
+        "color: yellow; font-weight: bold"
+      );
+      return success
+        ? { ...state, currentUser: registeredUser, token, username }
+        : { ...state, errorMessage: message };
+    case LOGIN_USER:
+      console.log(
+        `%c {type: LOGIN_USER, loggedInUser: ${JSON.stringify(loggedInUser)}} `,
+        "color: teal; font-weight: bold"
+      );
+      return success
+        ? { ...state, currentUser: loggedInUser, token, username }
+        : { ...state, errorMessage: message };
+    case LOGOUT_USER:
+      console.log(
+        `%c {type: LOGOUT_USER, currentUser: {}} `,
+        "color: pink; font-weight: bold"
+      );
+      return { ...state, currentUser: {} };
+    default:
+      return state;
+  }
+};
+
+export default UserReducer;
