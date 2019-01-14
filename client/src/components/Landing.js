3bdd148d2e2d50fe74b6eac9c8be72072ee817ec
Tucker <trblackw@ncsu.edu>
2019-01-14 12:46:21 -0500

    built out drag and drop logic and extracted it into its own utils file

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,28 +1,55 @@
 import React, { useContext, useState } from "react";
 import UserContext from "../state/context";
-import initialData from "./data";
+import initialData from "./drag&drop/data";
 import { DragDropContext } from "react-beautiful-dnd";
-import Column from "./Column";
+import Column from "./drag&drop/Column";
+import styled from "styled-components";
+import { onDragEnd, onDragStart, onDragUpdate } from "./drag&drop/utils";
 
 const Landing = () => {
   const { currentUser } = useContext(UserContext);
   const [state, setState] = useState(initialData);
 
-  const onDragEnd = (result) => {};
   return (
-    <div className="container border-solid border-blue p-3">
-      {/* <header className="p-3 mx-auto max-w-sm float-left">
-          <h3 className="text-4xl text-blue font-thin">Welcome back, <span className="font-bold">{currentUser.username}</span></h3> 
-        </header> */}
-      <DragDropContext onDragEnd={onDragEnd}>
-        {state.columnOrder.map(colId => {
-          const column = state.columns[colId];
-          const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
-          return <Column key={column.id} column={column} task={tasks} />;
-        })}
-      </DragDropContext>
-    </div>
+    <>
+      <Header className="container p-3 mb-10">
+        <header className="mt-2 mx-auto max-w-sm float-left mb-5">
+          <h3 className="text-2xl text-blue font-thin">
+            Welcome back,{" "}
+            <span className="font-bold">{currentUser.username}</span>
+          </h3>
+        </header>
+      </Header>
+      <LandingContainer className="border-solid border-blue">
+        <DragDropContext
+          onDragEnd={result => onDragEnd(result, state, setState)}
+          onDragUpdate={destination => onDragUpdate(destination, state)}
+          onDragStart={onDragStart}
+        >
+          {state.columnOrder.map(colId => {
+            const column = state.columns[colId];
+            const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+            return <Column key={column.id} column={column} task={tasks} />;
+          })}
+        </DragDropContext>
+      </LandingContainer>
+    </>
   );
 };
 
 export default Landing;
+
+const LandingContainer = styled("div")`
+  padding: 1em;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 90%;
+`;
+
+const Header = styled("div")`
+  margin: 0 2em;
+  width: auto;
+  padding: 0;
+`;
