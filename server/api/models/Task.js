e27c0847137af0fe45ff5e083a3bd86f4d64622d
Tucker <trblackw@ncsu.edu>
2019-01-14 21:44:45 -0500

    created Task model

--- server/api/models/Task.js
+++ server/api/models/Task.js
@@ -1,2 +1,33 @@
 const mongoose = require("mongoose"),
   Joi = require("joi");
+
+const taskSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    assigned: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
+
+// const validateTask = task => {
+//    const { author, assigned, _id } = task;
+
+// };
+
+// if (!mongoose.Types.ObjectId.isValid(req.body.id)) {
+//    return res.status(400).send("Invalid object id");
+// }
