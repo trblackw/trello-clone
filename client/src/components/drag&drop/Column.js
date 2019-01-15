d96558cf7a7832e461d3fa864d9ca6dc67f44516
Tucker <trblackw@ncsu.edu>
2019-01-15 15:25:56 -0500

    added toggling functionality for adding tasks

--- client/src/components/drag&drop/Column.js
+++ client/src/components/drag&drop/Column.js
@@ -2,27 +2,42 @@ import React from "react";
 import styled from "styled-components";
 import Task from "./Task";
 import TaskList from "./TaskList";
+import AddTask from "../AddTask";
+import useToggle from "../../hooks/useToggle";
 import { Droppable } from "react-beautiful-dnd";
 
-const Column = ({ column, task }) => (
-  <Container className="bg-blue-light rounded">
-    <Title>{column.title}</Title>
-    <Droppable droppableId={column.id}>
-      {(provided, snapshot) => (
-        <TaskList
-          provided={provided}
-          innerRef={provided.innerRef}
-          isDraggingOver={snapshot.isDraggingOver}
-        >
-          {task.map((task, index) => (
-            <Task key={index} task={task} index={index} />
-          ))}
-          {provided.placeholder}
-        </TaskList>
-      )}
-    </Droppable>
-  </Container>
-);
+const Column = ({ column, task }) => {
+  const [open, setOpen] = useToggle(false);
+  return (
+    <Container className="bg-blue-light rounded">
+      <Title>{column.title}</Title>
+      <Droppable droppableId={column.id}>
+        {(provided, snapshot) => (
+          <TaskList
+            provided={provided}
+            innerRef={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+          >
+            {task.map((task, index) => (
+              <Task key={index} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+            <AddTaskContainer>
+              <button
+                className="bg-green hover:bg-green-light text-white font-semibold hover:text-white py-2 px-4 ml-4 border shadow border-green-dark hover:border-transparent rounded flex"
+                onClick={() => setOpen(true)}
+              >
+                <img src="https:icon.now.sh/plus/fff" alt="plus icon" />
+                <span>Add task</span>
+              </button>
+              {open && <AddTask setOpen={setOpen} />}
+            </AddTaskContainer>
+          </TaskList>
+        )}
+      </Droppable>
+    </Container>
+  );
+};
 export default Column;
 
 const Container = styled("div")`
@@ -39,6 +54,12 @@ const Title = styled("h3")`
   font-size: 1.5em;
 `;
 
-// const TaskList = styled("div")`
-//   padding: 1em;
-// `;
+const AddTaskContainer = styled("div")`
+  border-radius: 5px;
+  width: 100%;
+  margin-bottom: 0.5em;
+
+  span {
+    margin-left: 0.5em;
+  }
+`;
