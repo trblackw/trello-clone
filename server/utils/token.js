06e144cf90632236ff30b094fab645dfa52c5188
Tucker <trblackw@ncsu.edu>
2019-01-15 08:54:47 -0500

    wrote some token middleware utils

--- server/utils/token.js
+++ server/utils/token.js
@@ -1,10 +1,42 @@
-const verifyToken = token => {
-  new Promise((resolve, reject) => {
-    jwt.verify(token, secrets.jwt, (err, payload) => {
-      if (err) return reject(err);
-      resolve(payload);
-    });
-  });
+const { secrets } = require("../config/keys"),
+  jwt = require("jsonwebtoken");
+
+const newToken = user => {
+  const token = jwt.sign(
+    { username: user.username, _id: user._id },
+    secrets.jwt,
+    {
+      expiresIn: secrets.jwtExp
+    }
+  );
+  return token;
 };
 
-module.exports = verifyToken;
+const verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
+
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, secrets.jwt, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: "invalid token"
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: "Auth token not supplied"
+    });
+  }
+};
+
+module.exports = { newToken, verifyToken };
