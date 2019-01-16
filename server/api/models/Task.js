a15700775326c07f756b0fd30f53b09538826113
Tucker <trblackw@ncsu.edu>
2019-01-16 15:04:47 -0500

    added title and description to Task model

--- server/api/models/Task.js
+++ server/api/models/Task.js
@@ -9,6 +9,16 @@ const taskSchema = new Schema(
       ref: "User",
       required: true
     },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      minlength: 10,
+      maxlength: 500,
+      required: true
+    },
     priority: {
       type: String
     },
@@ -24,19 +34,18 @@ const taskSchema = new Schema(
 
 const Task = mongoose.model("Task", taskSchema);
 
-
 const validateTask = task => {
-   if (!mongoose.Types.ObjectId.isValid(task._id)) {
-      throw new Error("Invalid task id");
-   }
-   const schema = {
-      priority: Joi.string()
+  if (!mongoose.Types.ObjectId.isValid(task._id)) {
+    throw new Error("Invalid task id");
+  }
+  const schema = {
+    priority: Joi.string()
       .alphanum()
       .min(2)
       .max(20)
-   };
-   
-   return Joi.validate(task, schema);
+  };
+
+  return Joi.validate(task, schema);
 };
 
 module.exports = { Task, validateTask };
