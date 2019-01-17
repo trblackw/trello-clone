16593fc7d319a829097af5b6dd1c725437f57451
Tucker <trblackw@ncsu.edu>
2019-01-17 18:48:59 -0500

    included boards in context instantiation for global use

--- client/src/state/context.js
+++ client/src/state/context.js
@@ -3,7 +3,8 @@ import { createContext } from "react";
 const UserContext = createContext({
   currentUser: localStorage.getItem("authenticated-user")
     ? JSON.parse(localStorage.getItem("authenticated-user"))
-    : {}
+      : {},
+   boards: []
 });
 
 export default UserContext;
