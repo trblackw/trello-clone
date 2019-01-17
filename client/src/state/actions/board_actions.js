df62edc5e77b59f5d0356a9b218786fdb656ac2d
Tucker <trblackw@ncsu.edu>
2019-01-17 09:40:18 -0500

    completed fetch board action

--- client/src/state/actions/board_actions.js
+++ client/src/state/actions/board_actions.js
@@ -1,14 +1,21 @@
-import { CREATE_BOARD } from "../constants";
+import { FETCH_BOARDS } from "../constants";
 
-export const createBoard = async user => {
+export const fetchBoards = async (user, dispatch) => {
   const { id, token } = user;
-  const res = await fetch(`http://localhost:8080/user/${id}`, {
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
+  try {
+    const res = await fetch(`http://localhost:8080/user/${id}/boards`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    });
+    const { success, boards } = await res.json();
+    if (success) {
+      console.log(boards);
+      return dispatch({ type: FETCH_BOARDS, boards });
     }
-  });
-  const { success, boards } = await res.json();
-  console.log({ success, boards });
+  } catch (error) {
+    console.error(error);
+  }
 };
