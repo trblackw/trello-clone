5bb2c178631a23594accae4924470c37f9164eab
Tucker <trblackw@ncsu.edu>
2019-01-17 09:39:27 -0500

    added boards to passed context

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React, { useContext, useReducer } from "react";
 import { Router } from "@reach/router";
 import Login from "./components/Login";
-import Landing from "./components/Landing";
 import UserContext from "./state/context";
 import UserReducer from "./state/reducer";
 import Nav from "./components/Nav";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import Board from "./components/Board";
 const App = () => {
   const initialState = useContext(UserContext);
-  const [{ currentUser }, dispatch] = useReducer(UserReducer, initialState);
+  const [{ currentUser, boards }, dispatch] = useReducer(
+    UserReducer,
+    initialState
+  );
   return (
     <>
-      <UserContext.Provider value={{ currentUser, dispatch }}>
+      <UserContext.Provider value={{ currentUser, boards, dispatch }}>
         <Nav />
         <Router>
           <Login path="/" />
           <Register path="/register" />
-          <Landing path="/user/:id" />
+          <Dashboard path="/user/:id" />
+          <Board path="/user/:id/:boardId" />
         </Router>
       </UserContext.Provider>
     </>
