e71e5cd5a2d2627e85b6d6a2b8e1dd0e0cd4c1b1
Tucker <trblackw@ncsu.edu>
2019-01-18 18:18:38 -0500

    added board action constant

--- client/src/state/constants.js
+++ client/src/state/constants.js
@@ -1,4 +1,8 @@
+//user
 export const REGISTER_USER = "REGISTER_USER";
 export const LOGIN_USER = "LOGIN_USER";
 export const LOGOUT_USER = "LOGOUT_USER";
 export const ERROR = "ERROR";
+
+//board
+export const FETCH_BOARDS = "CREATE_BOARD";
